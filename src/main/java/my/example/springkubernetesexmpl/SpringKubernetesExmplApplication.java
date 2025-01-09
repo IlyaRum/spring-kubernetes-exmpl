2a7b34c8fbcf4a4ca00efc85be90f6bc14409109
IlyaRum <tekstir@yandex.ru>
2025-01-09 17:21:38 +0300

    init commit

---
+++ src/main/java/my/example/springkubernetesexmpl/SpringKubernetesExmplApplication.java
@@ -0,0 +1,13 @@
+package my.example.springkubernetesexmpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubernetesExmplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKubernetesExmplApplication.class, args);
+    }
+
+}
